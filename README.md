a9a82058ee46fed25f7f275be4637a2b7488cacd
ainsleybc <ab.chang@me.com>
2017-10-25 14:57:53 +0100

    added up function

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 As a human living in a house
 So I can stay warm in winter
 I want a thermostat that starts at 20 degrees
+
+As a cold human,
+So I can be cosy,
+I want to increase the temperature of my thermostat
 ```
--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,8 +8,22 @@ describe('Thermostat', function () {
     thermostat = new Thermostat();
   })
 
-  it ('has default temperature of 20 degrees', function() {
-    expect(thermostat.temperature()).toEqual(20);
-  });
+  describe('temperature', function () {
+    
+    it ('has default temperature of 20 degrees', function() {
+      expect(thermostat.temperature()).toEqual(20);
+    });
+
+  })
+
+  describe('up', function () {
+    
+    it('increases the temperature by 1 degree', function () {
+      thermostat.up();
+      expect(thermostat.temperature()).toEqual(21);
+    })
+
+  })
+
 
 });
