da00a818d9ce1fd18f64340063a6cecb8431418e
Jon Sanders <jonsanders101@aol.com>
2017-10-26 21:26:36 +0100

    Add jQuery features

--- src/interface.js
+++ src/interface.js
@@ -2,26 +2,42 @@ $( document ).ready(function() {
 
   $('#temp_reading').html(thermo._temperature);
 
+  $('.energy_usage').html(thermo.energy_use());
+
   $( "#firebutton" ).click(function() {
-    $( "#weirdgif" ).toggle();
+    $( "#weird-gif" ).toggle();
   })
 
   $( "#up" ).click(function(){
     thermo.up();
     $('#temp_reading').html(thermo._temperature);
+    $('.energy_usage').html(thermo.energy_use());
   })
 
   $( "#down" ).click(function() {
     thermo.down();
     $('#temp_reading').html(thermo._temperature);
+    $('.energy_usage').html(thermo.energy_use());
   })
 
   $( "#reset" ).click(function() {
     thermo.reset();
     $('#temp_reading').html(thermo._temperature);
+    $('.energy_usage').html(thermo.energy_use());
+    $( "#psm_off" ).show();
+    $( "#psm_on" ).hide();
+  })
+
+  $( "#psm_off" ).click(function() {
+    thermo.powerSavingModeOff();
+    $( "#psm_off" ).toggle();
+    $( "#psm_on" ).toggle();
+  })
+
+  $( "#psm_on" ).click(function() {
+    thermo.powerSavingModeOn();
+    $( "#psm_off" ).toggle();
+    $( "#psm_on" ).toggle();
   })
 
 })
-
-
-$( "#weirdgif" ).hide();
